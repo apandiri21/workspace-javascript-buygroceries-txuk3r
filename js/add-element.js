8126cccaa8ce65fdfd3561b1f2e49622d95ebf15
apandiri21 <apandiri@usca.edu>
2020-11-02 13:17:17 -0500

    11/02 Assignment7exercise

--- js/add-element.js
+++ js/add-element.js
@@ -3,33 +3,26 @@
  */
 
 // get all the hot class elements- change them to cool class
-
+$("li.hot").removeClass("hot");
+$("li").addClass("cool");
 
 // traverse the elements
 
-
 // add a new element
-
 
 // add by clicking the plus sign
 document.getElementById("add").addEventListener("click", addElement);
 
 function addElement() {
   // add a new element
-  
 }
-
 
 //  click the li element will change the changeStyle
-function changeStyle() {
-
-}
+function changeStyle() {}
 
 // delete by clicking the trash can
 document.getElementById("remove").addEventListener("click", removeElement);
 
 function removeElement() {
   // remove the marked element
- 
- 
 }
